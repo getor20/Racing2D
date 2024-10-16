c8ffc2e58e6a35de3ffa87742f2dd2699f2cdf64
getor20 <ggetor20@gmail.com>
2024-10-16 19:07:02 +0200

    1

--- Assets/Scripts/Player/PlayerPlayerScript.cs
+++ Assets/Scripts/Player/PlayerPlayerScript.cs
@@ -3,19 +3,44 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
+
+    private float speed = 0f;
+
     private Vector2 vector2 = Vector2.zero;
 
     private void Awake()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        Debug.Log(vector2);
+        Debug.Log(speed);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(vector2.x * speed, vector2.y * speed);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         vector2 = context.ReadValue<Vector2>();
+
+        if (context.started)
+        {
+            while (true)
+            {
+                speed += 10f;
+            }        
+        }
+        else if (context.canceled)
+        {
+            while (true)
+            {
+                speed -= 10f;
+            }
+        }
     }
 }
